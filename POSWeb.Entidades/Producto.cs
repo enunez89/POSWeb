eb1914e22dfbcd3c11b8a91c18377c5ea128273b
enunezs89 <eddy.gerardo@gmail.com>
2019-07-09 15:15:42 -0600

    agrega clase producto

---
+++ POSWeb.Entidades/Producto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSWeb.Entidades
+{
+    /// <summary>
+    /// Clase de productos del inventario
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class Producto: BaseEntidad
+    {
+
+    }
+
+
+    #region RespuestasProducto
+    namespace POSWeb.Entidades
+    {
+        [DataContract]
+        public class RespuestaListaProducto : Paginacion
+        {
+            [DataMember]
+            public Respuesta Respuesta { get; set; }
+
+            [DataMember]
+            public List<Producto> ListaProducto { get; set; }
+        }
+
+        [DataContract]
+        public class RespuestaProducto
+        {
+            [DataMember]
+            public Respuesta Respuesta { get; set; }
+
+            [DataMember]
+            public Producto Producto { get; set; }
+        }
+    }
+    #endregion RespuestasProducto
+}
